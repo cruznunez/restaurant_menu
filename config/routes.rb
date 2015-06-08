1bf7466539f400f2542d1686ab8d8719a7868cd6
Cruz <nunez.a.cruz@gmail.com>
2015-06-08 14:58:31 -0400

    Gener8 Dish&&Course models, DishesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'dishes/index'
+
+  get 'dishes/show'
+
+  get 'dishes/new'
+
+  get 'dishes/edit'
+
+  get 'dishes/create'
+
+  get 'dishes/update'
+
+  get 'dishes/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
